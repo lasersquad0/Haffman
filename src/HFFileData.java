22757ffc85f67c21629bb6ac75e0a80e4a7a3c2c
a.romanchenko <lasersquad0@gmail.com>
2023-01-26 01:14:06 +0300

    added check for file format correctness

--- src/HFFileData.java
+++ src/HFFileData.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import static org.junit.Assert.*;
 
 public class HFFileData
 {
@@ -15,8 +16,8 @@ public class HFFileData
 9. Таблица символов — 2 * (количество знаков) байт [парами: символ — код]
 10. Сжатые данные — сколько уже получится байт
 */
-	private static final byte[] fileSignature = {'R', 'O', 'M', 'A'};
-	private static final byte[] fileFormat = {'0', '1'};
+	final byte[] fileSignature = {'R', 'O', 'M', 'A'};
+	final byte[] fileVersion = {'0', '1'};
 	long CRC32Value;
 	long fzUncompressed;
 	String fnUncompressed;
@@ -28,11 +29,41 @@ public class HFFileData
 	OutputStream sout;
 
 
+	public boolean checkHeaderData(boolean checkForCompressed)
+	{
+		byte[] b = {'R', 'O', 'M', 'A'};
+		assertArrayEquals(b, fileSignature);
+
+		b = new byte[]{'0', '1'};
+		assertTrue((fileVersion[0] >= '0') && (fileVersion[0] <= '9'));
+		assertTrue((fileVersion[1] >= '0') && (fileVersion[1] <= '9'));
+
+		assert CRC32Value > 0;
+		assert fzUncompressed > 0;
+
+		if(checkForCompressed)
+		{
+			assert fzCompressed > 0;
+			assert fnCompressed != null;
+			assert fnCompressed.length() > 0;
+			assert (lastBits > 0) && (lastBits <= 32); // lastBits==32 когда все биты в int значимые. Поэтому значение 0 не может быть здесь
+		}
+
+		assert fnUncompressed != null;
+		assert fnUncompressed.length() > 0;
+		assert tableSize > 0;
+
+		return true;
+	}
+
 	public void loadHeader() throws IOException
 	{
-		var dos = new DataInputStream(sin); // TODO добавить проверки что неожиданно не закнчился stream и т.п. (если подсунули кривой файл)
-		dos.read(fileSignature);  // TODO добавить проверку что это действительно правильный compressed file а не какой нить rar или zip
-		dos.read(fileFormat);
+		var dos = new DataInputStream(sin);
+		int res = dos.read(fileSignature);
+		res |= dos.read(fileVersion);
+		if(res < 0)
+			throw new IOException("Wrong file format.");
+
 		CRC32Value = dos.readLong();
 		fzCompressed = dos.readLong();
 		lastBits = dos.readByte();
@@ -48,13 +79,17 @@ public class HFFileData
 
 		fnUncompressed = sb.toString();
 		tableSize = dos.readShort();
+
+		checkHeaderData(false); // TODO здесь надо вообщем то true, но тогда тесты не работают так как они не могут постфактум записать fzCompressed и lastBits в произвольное место потока
 	}
 
 	public void saveHeader() throws IOException
 	{
+		checkHeaderData(false);
+
 		var dos = new DataOutputStream(sout);
 		dos.write(fileSignature);
-		dos.write(fileFormat);
+		dos.write(fileVersion);
 		dos.writeLong(CRC32Value);
 		dos.writeLong(fzCompressed);
 		dos.writeByte(lastBits);
@@ -66,7 +101,7 @@ public class HFFileData
 
 	public int encodedDataSizePos()
 	{
-		return fileSignature.length + fileFormat.length + Long.BYTES; // длина encoded файла записана по смещению 14 от начала файла архива
+		return fileSignature.length + fileVersion.length + Long.BYTES; // длина encoded файла записана по смещению 14 от начала файла архива
 	}
 
 }
