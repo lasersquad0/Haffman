0649f5212f2677a7c2097ff7ad1de3fb524243bd
lasersquad0 <lasersquad0@gmail.com>
2023-03-12 16:30:23 +0300

    added handling more uncompression methods

--- src/ArchiveManager.java
+++ src/ArchiveManager.java
@@ -6,20 +6,23 @@ public class ArchiveManager
 {
 	private final static Logger logger = Logger.getLogger(Utils.APP_LOGGER_NAME);
 
-	private static HashMap<Utils.CompressorTypes,Archiver> pool = null;
+	private static HashMap<Utils.CompTypes,Archiver> pool = null;
 
 	private static void initPool()
 	{
 		if(pool != null) return;
 
 		pool = new HashMap<>();
-		pool.put(Utils.CompressorTypes.NONE, null);
-		pool.put(Utils.CompressorTypes.HUFFMAN, new HFArchiver());
-		pool.put(Utils.CompressorTypes.ARITHMETIC, new RangeArchiver());
-		pool.put(Utils.CompressorTypes.RLE, new RLEArchiver());
-		pool.put(Utils.CompressorTypes.AARITHMETIC,new RangeAdaptArchiver());
-		pool.put(Utils.CompressorTypes.ARITHMETIC32, null);
-		pool.put(Utils.CompressorTypes.ARITHMETIC64, null);
+		pool.put(Utils.CompTypes.NONE, null);
+		pool.put(Utils.CompTypes.HUFFMAN, new HFArchiver());
+		pool.put(Utils.CompTypes.AHUFFMAN, new HFAdaptArchiver());
+		pool.put(Utils.CompTypes.RLE, new RLEArchiver());
+		pool.put(Utils.CompTypes.ARITHMETIC, new RangeArchiver());
+		pool.put(Utils.CompTypes.ARITHMETIC32, new RangeArchiver(Utils.CompTypes.ARITHMETIC32));
+		pool.put(Utils.CompTypes.ARITHMETIC64, new RangeArchiver(Utils.CompTypes.ARITHMETIC64));
+		pool.put(Utils.CompTypes.AARITHMETIC,  new RangeAdaptArchiver());
+		pool.put(Utils.CompTypes.AARITHMETIC32, new RangeAdaptArchiver(Utils.CompTypes.AARITHMETIC32));
+		pool.put(Utils.CompTypes.AARITHMETIC64, new RangeAdaptArchiver(Utils.CompTypes.AARITHMETIC64));
 	}
 
 	public static void uncompressFiles(String arcFilename) throws IOException
@@ -37,7 +40,7 @@ public class ArchiveManager
 		{
 			HFFileRec fr = fh.files.get(i);
 
-			Utils.CompressorTypes comp = Utils.CompressorTypes.values()[fr.alg];
+			Utils.CompTypes comp = Utils.CompTypes.values()[fr.alg];
 			Archiver arc = pool.get(comp);
 			arc.unCompressFile(fr, sin);
 		}
