4c078bfb00c62687c093ff678169bfca67ad1ccf
lasersquad0 <lasersquad0@gmail.com>
2023-03-12 16:36:15 +0300

    * some refactoring without change of logic

--- src/HFArchiver.java
+++ src/HFArchiver.java
@@ -4,9 +4,19 @@ import java.util.logging.*;
 public class HFArchiver extends Archiver
 {
 	private final static Logger logger = Logger.getLogger(Utils.APP_LOGGER_NAME);
-	static final Utils.CompressorTypes COMPRESSOR_CODE = Utils.CompressorTypes.HUFFMAN;
 	final int OUTPUT_BUF_SIZE = 100_000_000; // если файл >100M, то используем буфер этого размера иначе буфер размера файла
 
+	HFArchiver()
+	{
+		this(Utils.CompTypes.HUFFMAN);
+	}
+	HFArchiver(Utils.CompTypes compCode)
+	{
+		if(compCode != Utils.CompTypes.HUFFMAN)
+			throw new IllegalArgumentException(String.format("Incorrect compressor type '%s' is specified for '%s' compressor.", compCode.toString(), this.getClass().getName()));
+
+		COMPRESSOR_CODE = compCode;
+	}
 
 	@Override
 	public void compressFiles(String[] filenames) throws IOException
@@ -26,45 +36,47 @@ public class HFArchiver extends Archiver
 
 		for (int i = 0; i < fh.files.size(); i++)
 		{
-			HFFileRec fr = fh.files.get(i);
-
-			logger.info(String.format("Analysing file '%s'.", fr.origFilename));
-
-			File fl = new File(fr.origFilename);
-
-			int BUFFER = Utils.getOptimalBufferSize(fr.fileSize);
-			InputStream sin1 = new BufferedInputStream(new FileInputStream(fl), BUFFER); // stream только для подсчета весов и далее построения дерева Хаффмана
-
-			InputStream sin2 = new BufferedInputStream(new FileInputStream(fl), BUFFER);
-
-			HFTree tree = new HFTree();
-			tree.buildFromStream(sin1);
-			sin1.close();
-
-			fr.CRC32Value = tree.CRC32Value;
-
-			logger.info("Starting file compression...");
-
-			tree.saveTable(sout);
-
-			HFCompressData cData = new HFCompressData(sin2, sout, fr.fileSize);
-			HFCompressor c = new HFCompressor();
-
-			c.compress(tree, cData);
-
-			fr.compressedSize = cData.sizeCompressed;
-			fr.lastBits = cData.lastBits;
-
-			sin2.close();
-
-			printCompressionDone(fr);
-			//logger.info(String.format("Compression '%s' finished.", fr.origFilename));
+			compressFile(sout, fh.files.get(i));
 		}
 
 		sout.close();
 
 		fh.updateHeaders(arcFilename); // it is important to save info into files table that becomes available only after compression
 		logger.info("All files are processed.");
+	}
+
+	private void compressFile(OutputStream sout, HFFileRec fr) throws IOException
+	{
+		logger.info(String.format("Analysing file '%s'.", fr.origFilename));
+
+		File fl = new File(fr.origFilename);
+
+		int BUFFER = Utils.getOptimalBufferSize(fr.fileSize);
+		InputStream sin1 = new BufferedInputStream(new FileInputStream(fl), BUFFER); // stream только для подсчета весов и далее построения дерева Хаффмана
+
+		InputStream sin2 = new BufferedInputStream(new FileInputStream(fl), BUFFER);
+
+		HFTree tree = new HFTree();
+		tree.buildFromStream(sin1);
+		sin1.close();
+
+		fr.CRC32Value = tree.CRC32Value;
+
+		logger.info("Starting file compression...");
+
+		tree.saveTable(sout);
+
+		CompressData cData = new CompressData(sin2, sout, fr.fileSize, tree);
+		HFCompressor c = new HFCompressor();
+
+		c.compress(cData);
+
+		fr.compressedSize = cData.sizeCompressed;
+
+		sin2.close();
+
+		printCompressionDone(fr);
+		//logger.info(String.format("Compression '%s' finished.", fr.origFilename));
 	}
 
 	@Override
@@ -100,53 +112,18 @@ public class HFArchiver extends Archiver
 
 		OutputStream sout = new BufferedOutputStream(new FileOutputStream(fr.fileName), Utils.getOptimalBufferSize(fr.fileSize));
 
-		HFUncompressor uc = new HFUncompressor();
-		HFUncompressData uData = new HFUncompressData(sin, sout, fr.compressedSize, fr.fileSize, fr.lastBits);
+		var uc = new HFUncompressor();
+		var uData = new CompressData(sin, sout, fr.compressedSize, fr.fileSize, tree);
 
-		uc.uncompress(tree, uData);
+		uc.uncompress(uData);
 
 		if (uData.CRC32Value != fr.CRC32Value)
 			logger.warning(String.format("CRC values for file '%s' are not equal: %d and %d", fr.fileName, uData.CRC32Value, fr.CRC32Value));
 
 		sout.close();
 
-		logger.info(String.format("Extracting '%s' done.", fr.fileName));
+		logger.info(String.format("Extracting done '%s'.", fr.fileName));
 	}
 
-	/**
-	 * записываем в архив размер закодированного потока в байтах и lastBits для каждого файла в архиве
-	 * @param fh Header with correct compressedSize, lastBits and CRC32Values
-	 * @param arcFilename Name of the archive
-	 * @throws IOException if something goes wrong
-	 */
-	/*
-	@Override
-	public void updateHeaders(HFArchiveHeader fh, String arcFilename) throws IOException
-	{
-		RandomAccessFile raf = new RandomAccessFile(new File(arcFilename), "rw");
-
-		var offsets = fh.getFieldOffsets();
-
-		int pos = offsets.get(FHeaderOffs.InitialOffset);
-
-		for (int i = 0; i < fh.files.size(); i++)
-		{
-			HFFileRec fr = fh.files.get(i);
-
-			raf.seek(pos + offsets.get(FHeaderOffs.CRC32Value));
-			raf.writeLong(fr.CRC32Value);
-
-			raf.seek(pos + offsets.get(FHeaderOffs.compressedSize));
-			raf.writeLong(fr.compressedSize);
-
-			raf.seek(pos + offsets.get(FHeaderOffs.lastBits));
-			raf.writeByte(fr.lastBits);
-
-			pos = pos + offsets.get(FHeaderOffs.FileRecSize) + fr.fileName.length()*2; // length()*2 because writeChars() saves each char as 2 bytes
-		}
-
-		raf.close();
-	}
-*/
 }
 
