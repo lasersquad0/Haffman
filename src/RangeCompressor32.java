0c9a452e7b97814c7789ede99ccab6707ca27981
lasersquad0 <lasersquad0@gmail.com>
2023-03-12 16:45:09 +0300

    * many code refactorings * support 32 and 64 bit coding in one class. * no need for separate 32 and 64 bit identical classes

--- src/RangeCompressor32.java
+++ src/RangeCompressor32.java
@@ -1,65 +1,45 @@
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.logging.Logger;
 import java.util.zip.CRC32;
 
 public class RangeCompressor32 {
-	public enum Strategy { HIGHBYTE, RANGEBOTTOM }
 	private final static Logger logger = Logger.getLogger(Utils.APP_LOGGER_NAME);
 	private long low;
 	private long range;
-	private int nextCh;
-	static final int CODEBITS = 24;
-	static final long TOP = 1L << CODEBITS;
-	static final long TOPTOP = 1L << (CODEBITS + 8);
-	static final long BOTTOM = TOP >>> 8;
-	//final long INT_MASK = 0x00000000FFFFFFFFL;
-	private RangeCompressData cData;
+	int CODEBITS = 24;
+	int HIGHBYTE = CODEBITS + 8;
+	long TOP = 1L << CODEBITS;
+	long TOPTOP = 1L << (CODEBITS + 8);
+	long BOTTOM = TOP >>> 8;
+	private CompressData cData;
 	private final CRC32 crc = new CRC32();
 	public long encodedBytes = 0;
-	final long SHOW_PROGRESS_AFTER = 1_000_000; // display progress only if file size is larger then this
-	private final Strategy strategy; // you can use highbyte(low)==highbyte(high) strategy or range<BOTTOM (strategy==2)
 
+	public RangeCompressor32()
+	{
+		this(Utils.MODE.MODE32);  // by default use MODE32
+	}
 
-	RangeCompressor32(Strategy strategy)
+	public RangeCompressor32(Utils.MODE mode)
 	{
-		this.strategy = strategy;
+		if(mode == Utils.MODE.MODE32)
+			initConsts(24);
+		else
+			initConsts(48);
 	}
-	RangeCompressor32()
+
+	private void initConsts(int codebits)
 	{
-		this(Strategy.HIGHBYTE);
+		CODEBITS = codebits;
+		HIGHBYTE = CODEBITS + 8;
+		TOP      = 1L << CODEBITS;
+		TOPTOP   = 1L << (CODEBITS + 8);
+		BOTTOM   = 1L << (CODEBITS - 8);
 	}
-/*
+
 	public void compress(CompressData ccData) throws IOException
 	{
-		this.cData = (RangeCompressData) ccData;
-		assert cData.sizeUncompressed > 0;
-
-		encodedBytes = 0;  // сбрасываем счетчики
-
-		var rc = new RangeCoder();
-		Order0Coder o0c = new Order0Coder(rc);
-		int Symbol;
-
-		rc.StartEncode(cData.sout);
-
-		while((Symbol = cData.sin.read()) != -1)
-		//while ((Symbol = getc(DecodedFile)) >= 0)
-		{
-			o0c.encodeSymbol(Symbol);
-		}
-
-		o0c.stopEncode();
-
-		cData.sout.flush();
-
-		cData.sizeCompressed = encodedBytes = rc.getPassed();
-		cData.CRC32Value = crc.getValue();
-	}
-*/
-	public void compress(CompressData ccData) throws IOException
-	{
-		this.cData = (RangeCompressData) ccData;
+		this.cData = ccData;
 		assert cData.sizeUncompressed > 0;
 
 		startProgress();
@@ -69,74 +49,23 @@ public class RangeCompressor32 {
 
 		low = 0;
 		range = TOPTOP - 1; // сразу растягиваем на максимальный range
-		long totalFreq = cData.cumFreq[cData.cumFreq.length - 1];
+		//long totalFreq = cData.cumFreq[cData.cumFreq.length - 1];
 
-		logger.finer(()->String.format("freq=%s", Arrays.toString(cData.cumFreq)));
-		logger.finer(()->String.format("initial state: low=%X high=%X, range=%X", low, low + range, range));
+		//logger.finest(()->String.format("freq=%s", Arrays.toString(cData.cumFreq)));
+		logger.finest(()->String.format("initial state: low=%X high=%X, range=%X", low, low + range, range));
 
 		long total = 0;
-	//	int ch;
 
-		while((nextCh = cData.sin.read()) != -1)
+		int ch;
+		while((ch = cData.sin.read()) != -1)
 		{
-			crc.update(nextCh); // calculate CRC32 of *Uncompressed* file here
+			crc.update(ch); // calculate CRC32 of *Uncompressed* file here
 			updateProgress(++total);
 
-			int index = cData.symbol_to_freq_index[nextCh];
-			long left = cData.cumFreq[index - 1];
-			long right = cData.cumFreq[index];
-
-			assert(right <= totalFreq);
-			assert( (right-left) > 0);
-			assert(totalFreq <= BOTTOM);
-
-			range = range/totalFreq;
-			low = low + left * range;
-			assert (low & (TOPTOP - 1)) == low: "low is out of bounds!";
-			range =  (right - left) * range;
-
-			logger.finer(()->String.format("before scale sym=%X, low=%X high=%X, range=%X", nextCh, low, low + range, range));
-
-			boolean outByte = ((low ^ low + range) < TOP);
-			while( outByte || (range < BOTTOM) /*&& ((range= -low & BOTTOM-1),1)*/)
-			{
-				if( (!outByte) && (range < BOTTOM) ) // делает low+range=TOPTOP-1 если low+range было >TOPTOP-1
-				{
-					logger.finer( () -> String.format("Weird range correction:BEFORE range:%X, BOTTOM: %X", range, BOTTOM));
-					range = BOTTOM - (low & (BOTTOM - 1));
-					logger.finer( () -> String.format("Weird range correction:AFTER range:%X, BOTTOM: %X", range, BOTTOM));
-					assert range > 0;
-					assert (((low+range) >>> 32) == 0) || (((low+range) >>> 32) == 1):"low+range is out of bounds3!";
-				}
-
-				cData.sout.write((int)(low >>> 24));
-				encodedBytes++;
-				logger.finer(() ->String.format("Output byte: 0x%X, count: %,d", low >>> 24, encodedBytes));
-				range <<= 8;
-				assert (range & (TOPTOP-1)) == range: "range is out of bounds!";
-				low <<= 8;
-				low &= (TOPTOP - 1);
-				// если была хитрая коррекция выше тогда low+range==BOTTOM и после сдвига <<8, log+range==TOP, поэтому в assert добавлена проверка на ==1 это валидное значение
-				assert (((low+range) >>> 32) == 0) || (((low+range) >>> 32) == 1):"low+range is out of bounds2!";
-				outByte = ((low ^ low + range) < TOP);
-			}
-
-			logger.finer(()->String.format("AFTER  scale sym=%X, low=%X high=%X, range=%X", nextCh, low, low + range, range));
+			encodeSymbol(ch);
 		}
 
-		//low = low + range/2; // need value INSIDE the interval, take a middle
-		cData.sout.write((int)(low >>> 24));
-		encodedBytes++;
-		logger.finer(()->String.format("Output byte: 0x%X, count: %,d", low >>> 24, encodedBytes));
-		cData.sout.write((int) ((low >>> 16) & 0xFF));
-		encodedBytes++;
-		logger.finer(()->String.format("Output byte: 0x%X, count: %,d", (low >>> 16) & 0xFF, encodedBytes));
-		cData.sout.write((int) ((low >>> 8) & 0xFF));
-		encodedBytes++;
-		logger.finer(()->String.format("Output byte: 0x%X, count: %,d", (low >>> 8) & 0xFF, encodedBytes));
-		cData.sout.write((int) (low & 0xFF));
-		encodedBytes++;
-		logger.finer(()->String.format("Output byte: 0x%X, count: %,d", low & 0xFF, encodedBytes));
+		writeLastBytes();
 
 		cData.sout.flush();
 		finishProgress();
@@ -144,6 +73,74 @@ public class RangeCompressor32 {
 		cData.sizeCompressed = encodedBytes;
 		cData.CRC32Value = crc.getValue();
 	}
+
+	private void encodeSymbol(int sym) throws IOException
+	{
+		long[] res = cData.model.SymbolToFreqRange(sym);
+		//int index = cData.symbol_to_freq_index[sym];
+		long left = res[0]; //cData.cumFreq[index - 1];
+		long freq = res[1]; //cData.cumFreq[index];
+
+		assert (left + freq <= cData.model.totalFreq);
+		assert (freq > 0);
+		assert (cData.model.totalFreq <= BOTTOM);
+
+		//low = low + (left * range)/totalFreq;
+		//assert (low & (TOPTOP - 1)) == low: "low is out of bounds!";
+		//range = (right - left) * range/totalFreq;
+
+		range = range/ cData.model.totalFreq;
+		low = low + left * range;
+		assert (low & (TOPTOP - 1)) == low: "low is out of bounds!";
+		range = freq * range;
+
+		logger.finest(()->String.format("before scale sym=%X, low=%X high=%X, range=%X", sym, low, low + range, range));
+
+		boolean outByte = ((low ^ low + range) < TOP);
+		while( outByte || (range < BOTTOM) /*&& ((range= -low & BOTTOM-1),1)*/)
+		{
+			if( (!outByte) && (range < BOTTOM) ) // делает low+range=TOPTOP-1 если low+range было >TOPTOP-1
+			{
+				logger.finest( ()-> String.format("Weird range correction:BEFORE range:%X, BOTTOM: %X", range, BOTTOM));
+				range = BOTTOM - (low & (BOTTOM - 1));
+				logger.finest( () -> String.format("Weird range correction:AFTER range:%X, BOTTOM: %X", range, BOTTOM));
+				assert range > 0;
+				assert (((low+range) >>> HIGHBYTE) == 0) || (((low+range) >>> HIGHBYTE) == 1):"low+range is out of bounds3!";
+			}
+
+			int bwrite = (int)(low >>> CODEBITS);
+			writeByte(bwrite);
+			logger.finest(()->String.format("Output byte: 0x%X, count: %d", bwrite, encodedBytes));
+			//if(bwrite == 0) logger.fine(() ->String.format("Output byte is **ZERO**, count: %,d", encodedBytes));
+			range <<= 8;
+			assert (range & (TOPTOP-1)) == range: "range is out of bounds!";
+			low <<= 8;
+			low &= (TOPTOP - 1);
+			// если была хитрая коррекция выше тогда low+range==BOTTOM и после сдвига <<8, log+range==TOP, поэтому в assert добавлена проверка на ==1 это валидное значение
+			assert (((low+range) >>> HIGHBYTE) == 0) || (((low+range) >>> HIGHBYTE) == 1):"low+range is out of bounds2!";
+			outByte = ((low ^ low + range) < TOP);
+		}
+
+		logger.finest(()->String.format("AFTER  scale sym=%X, low=%X high=%X, range=%X", sym, low, low + range, range));
+	}
+
+	private void writeLastBytes() throws IOException
+	{
+		low = low + (range>>1); // need value INSIDE the interval, take a middle
+		for (int i = CODEBITS; i >= 0; i -= 8)
+		{
+			int ob = (int) ((low >> i) & 0xFF);
+			writeByte(ob);
+			logger.finest(()->String.format("Output byte: 0x%X, count: %,d", ob, encodedBytes));
+		}
+	}
+
+	public void writeByte(int b) throws IOException
+	{
+		cData.sout.write(b);
+		encodedBytes++;
+	}
+
 
 /*
 
@@ -167,7 +164,7 @@ public class RangeCompressor32 {
 	private long delta;
 	private void updateProgress(long total)
 	{
-		if((cData.sizeUncompressed > SHOW_PROGRESS_AFTER) && (total > threshold))
+		if((cData.sizeUncompressed > Utils.SHOW_PROGRESS_AFTER) && (total > threshold))
 		{
 			threshold += delta;
 			cData.cb.heartBeat((int)(100* threshold /cData.sizeUncompressed));
@@ -176,12 +173,12 @@ public class RangeCompressor32 {
 
 	private void finishProgress()
 	{
-		if(cData.sizeUncompressed > SHOW_PROGRESS_AFTER) cData.cb.finish();
+		if(cData.sizeUncompressed > Utils.SHOW_PROGRESS_AFTER) cData.cb.finish();
 	}
 
 	private void startProgress()
 	{
-		if(cData.sizeUncompressed > SHOW_PROGRESS_AFTER) cData.cb.start();
+		if(cData.sizeUncompressed > Utils.SHOW_PROGRESS_AFTER) cData.cb.start();
 	}
 
 
