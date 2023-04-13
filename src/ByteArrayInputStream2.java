d499241ff0ea9984d03932346fb07ac09f505f6d
a.romanchenko <lasersquad@gmail.com>
2023-04-14 00:41:06 +0300

    * added ByteArrayOutputStream2

--- src/ByteArrayInputStream2.java
+++ src/ByteArrayInputStream2.java
@@ -1,4 +1,5 @@
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class ByteArrayInputStream2 extends ByteArrayInputStream
 {
@@ -22,6 +23,30 @@ public class ByteArrayInputStream2 extends ByteArrayInputStream
 		buf = b;
 		pos = 0;
 		count = buf.length;
+	}
+
+	public byte[] getBuff()
+	{
+		return buf;
+	}
+}
+
+class ByteArrayOutputStream2 extends ByteArrayOutputStream
+{
+	public ByteArrayOutputStream2(byte[] buff)
+	{
+		buf = buff;
+		count = 0;
+	}
+	//public ByteArrayInputStream2(byte[] buf, int offset, int length)
+	//{
+	//	super(buf, offset, length);
+	//}
+
+	public void setNewBuff(byte[] b)
+	{
+		buf = b;
+		count = 0;
 	}
 
 	public byte[] getBuff()
