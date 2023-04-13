7bc80bcbc7aba16acfa8e878cd0272a09eb58df5
a.romanchenko <lasersquad@gmail.com>
2023-04-14 00:24:56 +0300

    * added two new methods

--- src/BlockCoderData.java
+++ src/BlockCoderData.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
+import java.io.InputStream;
 
 class BlockCoderData
 {
 	byte[] srcblock;
 	byte[] destblock;
-	private int blockSize; // initial block size, containing initial file data before first transformation
+	private int blockSize = 0; // initial block size, containing initial file data before first transformation
 	int cBlockSize; // compressed block size
 	int uBlockSize; // uncompressed block size, initialised during reading block from archive. in most cases uBlockSize==blockSize
 	int bytesInBlock = 0; // Needed for the case when last block read from a file. In all other cases bytesInBlock=inputblock.length
@@ -12,10 +14,31 @@ class BlockCoderData
 
 	public void allocate(int blockSize)
 	{
+		if(blockSize <= 0)
+			throw new IllegalArgumentException();
+
 		this.blockSize = blockSize;
 		srcblock = new byte[blockSize];
 		destblock = new byte[blockSize];
-		bytesInBlock = blockSize;
+		uBlockSize = blockSize;
+		bytesInBlock = 0;
+		ind = 0;
+	}
+
+	/**
+	 * Re-allocates buffers with new size newBlockSize
+	 * IMPORTANT - does not change values of int bwtLineNum, bytesInBlock, uBlockSize and cBlockSize since they may be defined before
+	 * reAllocate call because they've read from a file
+	 * @param newBlockSize - new size of this block
+	 */
+	public void reAllocate(int newBlockSize)
+	{
+		if(blockSize <= 0)
+			throw new IllegalArgumentException();
+
+		srcblock = new byte[newBlockSize];
+		destblock = new byte[newBlockSize];
+		blockSize = newBlockSize;
 		ind = 0;
 	}
 
@@ -45,4 +68,10 @@ class BlockCoderData
 		destblock = temp;
 	}
 
+	public int readFrom(InputStream in) throws IOException
+	{
+		bytesInBlock = in.read(srcblock, 0, blockSize);
+		return bytesInBlock;
+	}
 }
+
