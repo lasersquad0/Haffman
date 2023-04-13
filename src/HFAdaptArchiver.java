425e6633fb2747fdb9ff5ff2946dd331693e12ab
a.romanchenko <lasersquad@gmail.com>
2023-04-14 00:32:04 +0300

    * support option '-o' * support compress/uncompress in threads

--- src/HFAdaptArchiver.java
+++ src/HFAdaptArchiver.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
 public class HFAdaptArchiver extends Archiver
@@ -19,7 +20,7 @@ public class HFAdaptArchiver extends Archiver
 	}
 
 	@Override
-	public void compressFiles(String[] filenames) throws IOException
+	public String compressFiles(String[] filenames) throws IOException
 	{
 		if(filenames.length < 2)
 			throw new IllegalArgumentException("There are no files to compress. Exiting...");
@@ -44,33 +45,39 @@ public class HFAdaptArchiver extends Archiver
 		fh.updateHeaders(arcFilename); // it is important to save info into files table that becomes available only after compression
 
 		logger.info("All files are processed.");
+
+		return arcFilename;
 	}
 
 	private void compressFile(OutputStream sout, HFFileRec fr) throws IOException
 	{
 		logger.info(String.format("Starting compression '%s' ...", fr.origFilename));
 
-		var cp = new AdaptHuffman();
-		var bm = new BlockManager();
-		bm.compressFile(fr, sout, cp);
+		if(Utils.BLOCK_MODE)
+		{
+			var cp = new AdaptHuffman();
+			var bm = new BlockManager();
 
-		/*File fl = new File(fr.origFilename);
+			if (Utils.THREADS_COUNT > 1)
+				bm.compressFileInThread(fr, sout, cp);
+			else
+				bm.compressFile(fr, sout, cp);
+		}
+		else
+		{
+			InputStream sin = new BufferedInputStream(new FileInputStream(fr.origFilename), Utils.getOptimalBufferSize(fr.fileSize));
 
-		InputStream sin = new BufferedInputStream(new FileInputStream(fl), Utils.getOptimalBufferSize(fr.fileSize));
-		//InputStream fi1 = new BWTFilterInputStream(sin0, BLOCK_SIZE);
-		//InputStream sin = new MTFFilterInputStream(fi1, BLOCK_SIZE);
+			var cData = new CompressData(sin, sout, fr.fileSize);
+			var c = new AdaptHuffman();
 
-		var cData = new CompressData(sin, sout, fr.fileSize);
-		var c = new AdaptHuffman();
+			c.compress(cData);
 
-		c.compress(cData);
+			fr.compressedSize = cData.sizeCompressed;
+			fr.CRC32Value = cData.CRC32Value;
 
-		fr.compressedSize = cData.sizeCompressed;
-		//fr.lastBits = cData.lastBits;
-		fr.CRC32Value = cData.CRC32Value;
+			sin.close();
+		}
 
-		sin.close();
-*/
 		printCompressionDone(fr);
 		//logger.info(String.format("Compression '%s' finished.", fr.origFilename));
 	}
@@ -87,9 +94,6 @@ public class HFAdaptArchiver extends Archiver
 		HFArchiveHeader fh = new HFArchiveHeader();
 		fh.loadHeader(sin);
 
-		//var bm = new BlockManager();
-		//bm.uncompressFiles(fh, sin);
-
 		for (int i = 0; i < fh.files.size(); i++)
 		{
 			unCompressFile(fh.files.get(i), sin);
@@ -105,23 +109,32 @@ public class HFAdaptArchiver extends Archiver
 	{
 		logger.info(String.format("Extracting file '%s'...", fr.fileName));
 
-		OutputStream sout = new BufferedOutputStream(new FileOutputStream(fr.fileName), Utils.getOptimalBufferSize(fr.fileSize));
+		Path p = Path.of(Utils.OUTPUT_DIRECTORY, fr.fileName);
+		OutputStream sout = new BufferedOutputStream(new FileOutputStream(p.toFile()), Utils.getOptimalBufferSize(fr.fileSize));
 
-		var cp = new AdaptHuffman();
-		var bm = new BlockManager();
-		bm.uncompressFile(fr, sin, sout, cp);
+		if(Utils.BLOCK_MODE)
+		{
+			var cp = new AdaptHuffman();
+			var bm = new BlockManager();
+			var udata = new CompressData(sin, sout, fr.compressedSize, fr.fileSize);
+			bm.uncompressFile(fr, udata, cp);
 
-		/*
-		var uc = new AdaptHuffman();
-		var uData = new CompressData(sin, sout, fr.compressedSize, fr.fileSize);
+			if (udata.CRC32Value != fr.CRC32Value)
+				logger.warning(String.format("CRC values for file '%s' are not equal: %d and %d", fr.fileName, udata.CRC32Value, fr.CRC32Value));
+		}
+		else
+		{
+			var uc = new AdaptHuffman();
+			var uData = new CompressData(sin, sout, fr.compressedSize, fr.fileSize);
 
-		uc.uncompress(uData);
+			uc.uncompress(uData);
 
-		if (uData.CRC32Value != fr.CRC32Value)
-			logger.warning(String.format("CRC values for file '%s' are not equal: %d and %d", fr.fileName, uData.CRC32Value, fr.CRC32Value));
+			if (uData.CRC32Value != fr.CRC32Value)
+				logger.warning(String.format("CRC values for file '%s' are not equal: %d and %d", fr.fileName, uData.CRC32Value, fr.CRC32Value));
+		}
 
 		sout.close();
-*/
+
 		logger.info(String.format("Extracting done'%s'.", fr.fileName));
 	}
 
